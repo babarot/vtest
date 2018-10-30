94058e4f7c97062b7f1f09e4142baf5934655ab2
b4b4r07 <b4b4r07@gmail.com>
2018-10-30 13:35:03 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mcuadros/go-version"
+)
+
+func main() {
+	if len(os.Args[1:]) == 0 {
+		fmt.Fprintln(os.Stderr, "requires the following arguments. '0.12.40 > 0.12.37'")
+		os.Exit(1)
+	}
+
+	parts := strings.Split(os.Args[1], " ")
+	if len(parts) != 3 {
+		fmt.Fprintln(os.Stderr, "requires the following arguments. '0.12.40 > 0.12.37'")
+		os.Exit(1)
+	}
+
+	var (
+		v1       = parts[0]
+		v2       = parts[2]
+		operater = parts[1]
+	)
+
+	if !version.Compare(v1, v2, operater) {
+		os.Exit(1)
+	}
+}
